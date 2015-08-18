050fc11cc01a79a15292c332c08023926ed39bf2
lossyrob <rdemanuele@gmail.com>
2015-08-17 22:13:41 -0400

    Initial commit.

---
+++ cfn/utils/constants.py
@@ -0,0 +1,16 @@
+EC2_AVAILABILITY_ZONES = [
+    'us-west-2a',
+    'us-west-2b'
+]
+EC2_INSTANCE_TYPES = [
+    't2.micro',
+    't2.small',
+    't2.medium'
+]
+
+ALLOW_ALL_CIDR = '0.0.0.0/0'
+VPC_CIDR = '10.0.0.0/16'
+
+HTTP = 80
+HTTPS = 443
+SSH = 22
